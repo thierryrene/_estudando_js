bdacfdc1c7868e15d7e5006bf8e6f5591f28a1df
thierry rene matos <td_matos@terra.com.br>
2019-05-15 14:53:17 -0300

    first steps with requirejs - begin build workflow

--- _first_application/js/main.js
+++ _first_application/js/main.js
@@ -1,1 +1,47 @@
+require.config({
 
+    paths: {
+        'jquery':     'libs/jquery-1.11.2',
+        'underscore': 'libs/underscore',
+        'backbone':   'libs/backbone',
+        'bootstrap':  'libs/bootstrap.bundle'        
+    },
+
+    shim: {
+
+        'underscore': {
+            exports: '_'
+        },
+
+        'backbone': {
+            deps: [
+                'underscore',
+                'jquery'
+            ],
+            exports: 'Backbone'
+        },
+        
+        'bootstrap': {
+            deps: [
+                'jquery'
+            ]
+        }
+
+    }
+});
+
+require([
+    'backbone',
+    'app/app'
+], function (Backbone, App) {
+
+    'use strict';
+
+    console.log(Backbone, App);
+
+    App.initialize();
+    App.displayTime();
+    App.testFunction('thierry rene dos santos matos!');
+    App.testFunction2();
+
+});
